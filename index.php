8f2adaa8df73ac3c03755c789ecb079f2660c589
Esteban Gomez <109286055+Jinupse@users.noreply.github.com>
2025-02-27 09:46:52 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<head>
+    <title>Test</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="banner">
+        <a href="https://fr.wikipedia.org/wiki/Wikipédia:Accueil_principal">
+            <div class="d_logo">
+                <img id="image" src="https://moodle.utbm.fr/pluginfile.php/1/theme_boost_union/logocompact/300x300/1739884911/logo_utbm-blanc.png" alt="UTBM"><br>
+            </div>
+        </a>
+        <a href="https://fr.wikipedia.org/wiki/Wikipédia:Accueil_principal">
+            <div class="d_titre">
+                <p class="titre">Tableau de bord</p>
+            </div>
+        </a>
+        <a href="https://fr.wikipedia.org/wiki/Wikipédia:Accueil_principal">
+            <div class="d_titre">
+                <p class="titre">Mes cours</p>
+            </div>
+        </a>
+        <a href="https://fr.wikipedia.org/wiki/Wikipédia:Accueil_principal">
+            <div class="d_titre">
+                <p class="titre">Recherche de cours</p>
+            </div>
+        </a>
+        <a href="https://fr.wikipedia.org/wiki/Wikipédia:Accueil_principal">
+            <div class="d_titre">
+                <p class="titre">Sites UTBM</p>
+            </div>
+        </a>
+        <a href="https://fr.wikipedia.org/wiki/Wikipédia:Accueil_principal">
+            <div class="d_titre">
+                <p class="titre">Semestre à l'UTBM</p>
+            </div>
+        </a>
+        <a href="https://fr.wikipedia.org/wiki/Wikipédia:Accueil_principal">
+            <div class="d_titre">
+                <p class="titre">Stages/S.E.E.</p>
+            </div>
+        </a>
+    </div>
+    <p class="s_titre">Coucou</p>
+</body>
